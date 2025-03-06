9465cc2cd96ec2f76a5974b83636aefb47b31a9e
angeliani <angeliani.junitaviana@gmail.com>
2025-03-06 11:28:32 +0700

    Main.java

--- tugas1angel/src/skincare/Main.java
+++ tugas1angel/src/skincare/Main.java
@@ -1,30 +1,29 @@
 package skincare;
 
-import skincare.SkinType;  // Import kelas SkinType dari package skincare
+import skincare.SkinType;  
 
 public class Main {
     public static void main(String[] args) {
         // Deklarasi dan instansiasi objek
         SkinType angelSkin1 = new SkinType();
-        SkinType skinDidi = new SkinType();  // Mengganti angelSkin2 dengan skinDidi
+        SkinType skinDidi = new SkinType();  
         
-        // Set nilai atribut untuk angelSkin1 (untuk Angel)
+        // Set nilai atribut 
         angelSkin1.kulitAngel = "Kering";  // Atribut untuk Angel
         angelSkin1.undertoneAngel = "Warm";
         angelSkin1.sensitivitasAngel = "Normal";
         
-        // Set nilai atribut untuk skinDidi (untuk Didi)
         skinDidi.kulitAngel = "Berminyak";  // Atribut untuk Didi
         skinDidi.undertoneAngel = "Neutral";
         skinDidi.sensitivitasAngel = "Sensitif";
         
         // Tampilkan informasi
         System.out.println("Skin Type Pertama (Angel):");
-        angelSkin1.tampilkanInfo("Angel");  // Pass "Angel" for the first skin type
+        angelSkin1.tampilkanInfo("Angel"); 
         
         System.out.println();
         
         System.out.println("Skin Type Kedua (Didi):");
-        skinDidi.tampilkanInfo("Didi");  // Pass "Didi" for the second skin type
+        skinDidi.tampilkanInfo("Didi");  
     }
 }
